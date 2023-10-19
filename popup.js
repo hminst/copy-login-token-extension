01c766dc3cedf98534721f41443942ae49426bb0
Helmut Minst <helmut.minst@valtech.com>
2023-10-19 15:05:38 +0200

    Revert "toggle wcmmode. show buttons conditionally whether they are functional in context"
    This reverts commit 137920c379cf744413f81c643f0dac9e1d32f28b.

--- popup.js
+++ popup.js
@@ -1,73 +1,46 @@
-const tokenButton = document.getElementById('token');
-const wcmmodeButton = document.getElementById('wcmmode');
+const form = document.getElementById('control-row');
 const message = document.getElementById('message');
 
 const LOGIN_TOKEN_COOKIE_NAME = 'login-token';
-const WCMMODE_COOKIE_NAME = 'wcmmode';
-
-let currentUrl;
 
 // The async IIFE is necessary because Chrome <89 does not support top level await.
 (async function initPopupWindow() {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  if (tab?.url) {
-    currentUrl = new URL(tab.url);
-    if (!currentUrl) {
-      throw Error('url could not be identified');
-    }
-    if (currentUrl.hostname == 'localhost') {
-      tokenButton.remove();
-    } else {
-      wcmmodeButton.remove();
-    }
 
-  }
-  updateWCMmodeButton();
 })();
 
-tokenButton.addEventListener('click', handleTokenSync);
-wcmmodeButton.addEventListener('click', handleWCMMode);
+form.addEventListener('submit', handleFormSubmit);
 
-async function handleWCMMode(event) {
+async function handleFormSubmit(event) {
   event.preventDefault();
-  let wcmmode = 'disabled'
-  const wcmmodeCookie = await chrome.cookies.get({ url: currentUrl.origin, name: WCMMODE_COOKIE_NAME })
-  if (wcmmodeCookie) {
-    if (wcmmodeCookie.value != 'enabled') {
-      wcmmode = 'enabled'
-    } else {
-      wcmmode = 'disabled'
+
+  clearMessage();
+
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  if (tab?.url) {
+    try {
+      let url = new URL(tab.url);
+
+      if (!url) {
+        setMessage('Invalid URL');
+        return;
+      }
+
+      let message = await copyLoginToken(url.origin);
+      setMessage(message);
+    } catch {
+      // ignore
     }
+
   }
-
-  chrome.cookies.set(
-    { domain: currentUrl.hostname, name: WCMMODE_COOKIE_NAME, value: wcmmode, url: currentUrl.origin, path: '/' }
-  );
-
-  updateWCMmodeButton();
 }
 
-
-async function updateWCMmodeButton() {
-  const wcmmodeCookie = await chrome.cookies.get({ url: currentUrl.origin, name: WCMMODE_COOKIE_NAME })
-
-  if (wcmmodeCookie) {
-    wcmmodeButton.innerText = `WCMMODE=${wcmmodeCookie.value}`;
-  } else {
-    wcmmodeButton.innerText = `WCMMODE=?`;
-  }
-  var code = 'window.location.reload();';
-  chrome.scripting.executeScript(code)
-}
-
-async function handleTokenSync(event) {
-  event.preventDefault();
-
+async function copyLoginToken(origin) {
   try {
     const loginTokenCookie = await chrome.cookies.get({ url: origin, name: LOGIN_TOKEN_COOKIE_NAME })
 
     if (!loginTokenCookie) {
-      setMessage(tokenButton, 'login-token not found!')
+      return 'login-token not found!'
     }
     const loginTokenValue = loginTokenCookie.value;
     chrome.cookies.set(
@@ -75,17 +48,17 @@ async function handleTokenSync(event) {
     )
 
   } catch (error) {
-    setMessage(tokenButton, `Unexpected error: ${error.message}`)
-
+    return `Unexpected error: ${error.message}`;
   }
-  setMessage(tokenButton, 'token synchornized')
-
+  return `token copied!`;
 }
 
-async function copyLoginToken(origin) {
-
+function setMessage(str) {
+  message.textContent = str;
+  message.hidden = false;
 }
 
-function setMessage(element, str) {
-  tokenButton.textContent = str;
+function clearMessage() {
+  message.hidden = true;
+  message.textContent = '';
 }
